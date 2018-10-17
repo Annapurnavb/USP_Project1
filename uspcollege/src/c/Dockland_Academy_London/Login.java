eba97603b4d6782c71babc37ac9d98b1ec1962d9
annpurnavb <Annapurna.Badiger@precedent.com>
2018-10-17 16:25:17 +0100

    commit

---
+++ uspcollege/src/c/Dockland_Academy_London/Login.java
@@ -0,0 +1,35 @@
+package c.Dockland_Academy_London;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Login {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver;
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\Annapurna.Badiger\\eclipse-workspace\\ann\\uspcollege\\jars\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+		driver.get("https://umbraco.uspcollege.ac.uk/");
+		driver.manage().window().maximize();
+		driver.findElement(By.cssSelector("input[value='Accept']")).click();
+
+		driver.findElement(By.xpath("/html/body/header/nav/ul[3]/li[2]/a")).click();
+		driver.findElement(By.xpath("/html/body/header/nav/ul[3]/li[2]/ul/li[1]/a")).click();
+		driver.findElement(By.cssSelector("#EmailAddress")).sendKeys("ssd@gmail.com");
+		driver.findElement(By.cssSelector("#Password")).sendKeys("ssdw1234");
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy(0,500)");
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//*[@id=\"mainContent\"]/div/div/div/form/p[2]/input")).click();
+		driver.quit();
+
+	}
+
+}
